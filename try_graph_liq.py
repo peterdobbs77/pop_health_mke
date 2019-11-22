62580ceed4f56f54772d045fe5051bf16f652dd2
Peter Dobbs <dobbsp8@gmail.com>
2019-11-22 11:37:14 -0600

    trying to plot data on map

--- try_graph_liq.py
+++ try_graph_liq.py
@@ -4,14 +4,15 @@ import pandas as pd
 import numpy as np
 from geopy.geocoders import Nominatim
 from matplotlib import pyplot as plt
+from shapely.geometry import Point, Polygon
 
 # API call to Open MKE Data for liquor license data
-url = "https://data.milwaukee.gov/api/3/action/datastore_search?resource_id=45c027b5-fa66-4de2-aa7e-d9314292093d&limit=200"
+url = "https://data.milwaukee.gov/api/3/action/datastore_search?resource_id=45c027b5-fa66-4de2-aa7e-d9314292093d&limit=5"
 r = requests.get(url).json()
+print(json.dumps(r, indent=4))
 print('Data query success:', r['success'])
 
 if(not r['success']):
-    print(json.dumps(r, indent=4))
     exit()
 
 # If data query is successful, let's play with the data!
@@ -26,8 +27,13 @@ address = df[['HOUSE_NR', 'SDIR', 'STREET', 'STTYPE']].apply(
 # print(address)
 
 
+fig = plt.figure()
+plt.title('MKE Liqour Licenses')
+ax = plt.axes()
+ax.set_aspect('equal')
+
 geolocator = Nominatim(user_agent="App_Name")
-location = geolocator.geocode(address[0])
-print(location.address)
-print((location.latitude, location.longitude))
-print(location.raw)
+pts = []
+for loc in address:
+    geoloc = geolocator.geocode(loc)
+    pts.append(Point((geoloc.longitude, geoloc.latitude))
