25e8fae2ad7476e37255033c08d6637c48360a4c
Peter Dobbs <dobbsp8@gmail.com>
2019-11-24 16:38:04 -0600

    guess I made a lot of changes I forgot to commit

--- try_graph_liq.py
+++ try_graph_liq.py
@@ -7,28 +7,23 @@ from geopy.geocoders import Nominatim
 from matplotlib import pyplot as plt
 from shapely.geometry import Point, Polygon
 from descartes.patch import PolygonPatch
+from datetime import datetime
 
 
-def plot_locations_on_mke_ald_map(loc_bbox):
-    # Initialize shape file for Milwaukee Aldermanic Districts
-    ald_shp = shapefile.Reader("./shape/ald2018/alderman")
+def plot_shape(shp_path):
+    shp = shapefile.Reader(shp_path)
 
     fig = plt.figure(figsize=(12, 10))
-    plt.title(df.columns[0])
     ax = plt.axes()
     ax.set_aspect('equal')
 
-    for i, shape in enumerate(ald_shp.shapes()):
-        # define polygon fill color (facecolor) RGB values:
-        R = 1
-        G = 1
-        B = 0.2
-        # check number of parts (could use MultiPolygon class of shapely?)
+    for i, shape in enumerate(shp.shapes()):
+        # check number of parts (could try MultiPolygon class of shapely?)
         nparts = len(shape.parts)  # total parts
         if nparts == 1:
             polygon = Polygon(shape.points)
             patch = PolygonPatch(polygon, facecolor=[
-                                 R, G, B], alpha=1.0, zorder=2)
+                                 1, 1, 1], alpha=1.0, zorder=0)
             ax.add_patch(patch)
         else:  # loop over parts of each shape, plot separately
             for ip in range(nparts):  # loop over parts, plot separately
@@ -39,61 +34,131 @@ def plot_locations_on_mke_ald_map(loc_bbox):
                     i1 = len(shape.points)
                 polygon = Polygon(shape.points[i0:i1+1])
                 patch = PolygonPatch(polygon, facecolor=[
-                                     R, G, B], alpha=1.0, zorder=2)
+                                     1, 1, 1], alpha=1.0, zorder=0)
                 ax.add_patch(patch)
-        # annotate the shape with aldermanic district number
-        center_x = shape.bbox[0]+((shape.bbox[2]-shape.bbox[0])/2)
-        center_y = shape.bbox[1]+((shape.bbox[3]-shape.bbox[1])/2)
-        ax.annotate(ald_shp.record(i)[1], xy=(center_x, center_y), color='tan',
-                    ha='center', va='center', fontsize=21)
+        # annotate the shape with label in data
+        # center_x = shape.bbox[0]+((shape.bbox[2]-shape.bbox[0])/2)
+        # center_y = shape.bbox[1]+((shape.bbox[3]-shape.bbox[1])/2)
+        # ax.annotate(shp.record(i)[1], xy=(center_x, center_y), color='tan',
+        #             ha='center', va='center', fontsize=21)
 
-    for bbox in loc_bbox:
-        p1 = Point(bbox[0], bbox[2])
-        p2 = Point(bbox[1], bbox[2])
-        p3 = Point(bbox[0], bbox[3])
-        p4 = Point(bbox[1], bbox[3])
-        pointList = [p1, p2, p3, p4]
-        polygon = Polygon([[p.x, p.y] for p in pointList])
-        patch = PolygonPatch(polygon, facecolor=[
-                             0.1, 1, 1], alpha=1.0, zorder=2)
-        ax.scatter(patch)
-
-    plt.xlim(ald_shp.bbox[0], ald_shp.bbox[2])
-    plt.ylim(ald_shp.bbox[1], ald_shp.bbox[3])
     plt.axis('off')
     plt.show()
-    fig.savefig('./images/loc_on_2018_alddist.png')
+    fig.savefig(
+        './output/plot_shape_{}.png'.format(datetime.timestamp(datetime.now())))
+
+
+def plot_locations_on_mke_map(points):
+    print('..start plot generation..')
+    # Initialize shape file for Milwaukee County Municipal Boundaries
+    shp = shapefile.Reader("./shape/citylimit/citylimit")
+
+    fig = plt.figure(figsize=(15, 12))
+    plt.title('City of Milwaukee')
+    ax = plt.axes()
+    ax.set_aspect('equal')
+
+    for i, shape in enumerate(shp.shapes()):
+        # check number of parts (could try MultiPolygon class of shapely?)
+        nparts = len(shape.parts)  # total parts
+        if nparts == 1:
+            polygon = Polygon(shape.points)
+            patch = PolygonPatch(polygon, facecolor=[
+                                 1, 1, 1], alpha=1.0, zorder=0)
+            ax.add_patch(patch)
+        else:  # loop over parts of each shape, plot separately
+            for ip in range(nparts):  # loop over parts, plot separately
+                i0 = shape.parts[ip]
+                if ip < nparts-1:
+                    i1 = shape.parts[ip+1]-1
+                else:
+                    i1 = len(shape.points)
+                polygon = Polygon(shape.points[i0:i1+1])
+                patch = PolygonPatch(polygon, facecolor=[
+                                     1, 1, 1], alpha=1.0, zorder=0)
+                ax.add_patch(patch)
+        # annotate the shape with label in data
+        # center_x = shape.bbox[0]+((shape.bbox[2]-shape.bbox[0])/2)
+        # center_y = shape.bbox[1]+((shape.bbox[3]-shape.bbox[1])/2)
+        # ax.annotate(shp.record(i)[1], xy=(center_x, center_y), color='tan',
+        #             ha='center', va='center', fontsize=21)
+
+    # ADD ADDRESS GEOCOORDINATES AS POINTS
+    plt.scatter(points.x, points.y, s=20, c="cyan")
+
+    # SET VIEW TO WITHIN EXPECTED BOUNDS OF CITY OF MKE
+    # plt.xlim(shp.bbox[0], shp.bbox[2])
+    # plt.ylim(shp.bbox[1], shp.bbox[3])
+
+    #
+    plt.axis('on')
+    plt.show()
+    fig.savefig('./output/{}.png'.format(datetime.timestamp(datetime.now())))
+
+
+useApi = False
+limitApi = 400
+
+# plot_shape('./shape/citylimit/citylimit')
+
+df = pd.read_csv('./data/liquorlicenses.csv')
+
+if (useApi):
+    resourceId = '45c027b5-fa66-4de2-aa7e-d9314292093d'
+    # API call to Open MKE Data for liquor license data
+    url = "https://data.milwaukee.gov/api/3/action/datastore_search?resource_id={}&limit={}".format(resourceId,
+                                                                                                    limitApi)
+    r = requests.get(url).json()
+    # print(json.dumps(r, indent=4))
+    print('Data query success:', r['success'])
+
+    if(not r['success']):
+        exit()
+
+    # If data query is successful, let's play with the data!
+    data = r['result']
+    df = pd.DataFrame(data['records'])
+
+df['CITY'] = "Milwaukee, WI"  # add city, state to help geo location
+print(df.head())
+
+# df_grouped = df[['']]
+
+address = df[['HOUSE_NR', 'SDIR', 'STREET', 'STTYPE', 'CITY']].apply(
+    lambda x: ' '.join(map(str, x)), axis=1)
+print(address)
 
 
 # Initialize geolocator tool that uses OpenMapData
-geolocator = Nominatim(user_agent="App_Name")
-# API call to Open MKE Data for liquor license data
-url = "https://data.milwaukee.gov/api/3/action/datastore_search?resource_id=45c027b5-fa66-4de2-aa7e-d9314292093d&limit=5"
-r = requests.get(url).json()
-# print(json.dumps(r, indent=4))
-print('Data query success:', r['success'])
-
-if(not r['success']):
-    exit()
-
-# If data query is successful, let's play with the data!
-data = r['result']
-df = pd.DataFrame(data['records'])
-df['CITY'] = "Milwaukee, WI"  # add city, state to help geo location
-# print(df)
-
-address = df[['HOUSE_NR', 'SDIR', 'STREET', 'STTYPE', 'CITY']].apply(
-    lambda x: ' '.join(x), axis=1)
-# print(address)
-
+geolocator = Nominatim(user_agent="App_Name5")
+coords = []
 points = []
-for loc in address:
+for i, loc in enumerate(address):
+    if(i > limitApi):
+        break
+
     geoloc = geolocator.geocode(loc)
-    print(geoloc.raw)
-    # bounding_box = geoloc.raw['boundingbox']
-    # lat_long_bbox.append(bounding_box)
-    points.append(Point(geoloc.longitude, geoloc.latitude))
+    if(geoloc is None):
+        print(i, '..error: no location found for input address! ({})'.format(loc))
+        coords.append({'lat': '',
+                       'lon': ''})
+        points.append({'x': '',
+                       'y': ''})
+        continue
 
-print(points)
+    # print(geoloc.raw)
+    # print(i, ":", (geoloc.longitude, geoloc.latitude))
+    coords.append({'lat': geoloc.latitude,
+                   'lon': geoloc.longitude})
+    points.append({'x': (geoloc.longitude*10000.0)+3600000,
+                   'y': geoloc.latitude*10000.0})
 
-plot_locations_on_mke_ald_map(points)
+crds = pd.DataFrame(coords)
+df['LAT'] = crds['lat']
+df['LON'] = crds['lon']
+df.to_csv('./output/liquorlicenses_mod.csv')
+
+pts = pd.DataFrame(points)
+print(pts)
+
+plot_locations_on_mke_map(pts)
